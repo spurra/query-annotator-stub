7448d328aa9c82763f746a674ba99b951939cd24
Adrian Spurr <adrian@public-docking-cx-2585.ethz.ch>
2016-04-06 18:29:57 +0200

    First draft of baseline-1 implementation

--- src/main/java/annotatorstub/annotator/FakeAnnotator.java
+++ src/main/java/annotatorstub/annotator/FakeAnnotator.java
@@ -13,9 +13,13 @@ import it.unipi.di.acube.batframework.utils.AnnotationException;
 import it.unipi.di.acube.batframework.utils.ProblemReduction;
 import it.unipi.di.acube.batframework.utils.WikipediaApiInterface;
 
+import it.unipi.di.acube.batframework.problems.A2WDataset;
+
 public class FakeAnnotator implements Sa2WSystem {
 	private static long lastTime = -1;
 	private static float threshold = -1f;
+
+	private static A2WDataset trainingData;
 	
 	public long getLastAnnotationTime() {
 		return lastTime;
@@ -39,12 +43,34 @@ public class FakeAnnotator implements Sa2WSystem {
 
 	public HashSet<ScoredAnnotation> solveSa2W(String text) throws AnnotationException {
 		lastTime = System.currentTimeMillis();
-		int start = 0;
-		while (start < text.length() && !Character.isAlphabetic(text.charAt(start)))
-			start++;
-		int end = start;
-		while (end < text.length() && Character.isAlphabetic(text.charAt(end)))
-			end++;
+
+		String[] words = text.split(" ");
+		int n = words.length;
+
+		// Iterate through all possible mentions and check if it exists in the training set.
+		// Start with the longest.
+		int start, end;
+		HashSet<ScoredAnnotation> result = new HashSet<>();
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j <= i; j++) {
+				// TODO adjust index for substring
+				int left_index = i;
+				int right_index = n - i + j;
+
+				String extract = text.substring(left_index, right_index);
+				int id = checkMention(extract);
+
+				if (id != -1)
+					result.add(new ScoredAnnotation(left_index, length_of_mention, id, 0.1f));
+
+
+			}
+		}
+
+
+
+
 		
 		int wid;
         try {
@@ -53,12 +79,23 @@ public class FakeAnnotator implements Sa2WSystem {
 	        throw new AnnotationException(e.getMessage());
         }
 		
-		HashSet<ScoredAnnotation> result = new HashSet<>();
+
 		if (wid != -1)
 			result.add(new ScoredAnnotation(start, end - start, wid, 0.1f));
 		lastTime = System.currentTimeMillis() - lastTime;
 		return result;
     }
+
+	public void setTrainingData(A2WDataset data) {
+		trainingData = data;
+	}
+
+	private int checkMention(String mention) {
+		// TODO Check training set for mention. If found, return id of wikipedia article
+
+
+		return -1;
+	}
 	
 	public String getName() {
 		return "Simple yet uneffective query annotator";
