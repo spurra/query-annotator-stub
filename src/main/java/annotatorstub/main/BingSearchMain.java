15aba7b1ad9fab9678ac99e56585706c9c9d9aca
Michael Gygli <gygli@vision.ee.ethz.ch>
2016-04-29 17:18:04 +0200

    add api key to bing class

--- src/main/java/annotatorstub/main/BingSearchMain.java
+++ src/main/java/annotatorstub/main/BingSearchMain.java
@@ -1,3 +1,9 @@
+/*
+ * Bing account
+ * mail: gygli@vision.ee.ethz.ch
+ * pwd: Placeholder
+ * key: jRstdZaO2NyTyCDBnXkl2PAXeXSGksYjM1T20XXuxa8
+ */
 package annotatorstub.main;
 
 import org.codehaus.jettison.json.JSONObject;
@@ -6,8 +12,8 @@ import it.unipi.di.acube.BingInterface;
 
 public class BingSearchMain {
 	public static void main(String[] args) throws Exception {
-		BingInterface bing = new BingInterface("<your key goes here>");
-		JSONObject a = bing.queryBing("funny kittens");
+		BingInterface bing = new BingInterface("jRstdZaO2NyTyCDBnXkl2PAXeXSGksYjM1T20XXuxa8");
+		JSONObject a = bing.queryBing("funy kittens wikipedia");
 
 		// see: http://datamarket.azure.com/dataset/bing/search#schema for
 		// query/response format
