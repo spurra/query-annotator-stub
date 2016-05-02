de0049bdc076fcc72d853f9a5bdc58d0335a1e5e
Michael Gygli <gygli@vision.ee.ethz.ch>
2016-05-02 16:26:13 +0200

    add code to do spelling correction using Bing

--- src/main/java/annotatorstub/main/AnnotatorMain.java
+++ src/main/java/annotatorstub/main/AnnotatorMain.java
@@ -11,7 +11,8 @@ import annotatorstub.annotator.FakeAnnotator;
 public class AnnotatorMain {
 
 	public static void main(String[] args) throws IOException {
-		FakeAnnotator ann = new FakeAnnotator(null);
+		WikipediaApiInterface wikiApi = WikipediaApiInterface.api();
+		FakeAnnotator ann = new FakeAnnotator(wikiApi);
 		String query = "strawberry fields forever";
 		HashSet<Annotation> annotations = ann.solveA2W(query);
 		for (Annotation a : annotations) {
