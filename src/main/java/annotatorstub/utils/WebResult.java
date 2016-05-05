42f1f79efdedb8829f7d678fe3ba5870f499921f
Michael Gygli <gygli@vision.ee.ethz.ch>
2016-05-05 16:20:22 +0200

    webresult stores entity if is wikipage

--- src/main/java/annotatorstub/utils/WebResult.java
+++ src/main/java/annotatorstub/utils/WebResult.java
@@ -1,17 +1,23 @@
 package annotatorstub.utils;
+import java.util.regex.Pattern;
 
 public class WebResult {
 	public String title;
 	public String description;
 	public String url;
+	public String entity;
 	
 	public WebResult(String title, String description, String url) {
 		this.title=title;
 		this.url=url;
 		this.description=description;
+		if (url.startsWith("https://en.wikipedia.org/wiki/")) {
+			this.entity = url.replace("https://en.wikipedia.org/wiki/", "");
+		}
+		
 	}
 	
 	public String toString() { 
-	    return "title: '" + this.title + "', url: '" + this.url + "', description: '" + this.description + "'";
+	    return "title: '" + this.title + "', entity: '" + this.entity + "', url: '" + this.url + "', description: '" + this.description + "'";
 	} 
 }
