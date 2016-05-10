ede34de4574e1b8bbdc6bc712e0d9cbf3b9fe973
mircicd <mircicd@student.ethz.ch>
2016-05-10 12:30:33 +0200

    svm classifier f1 measure model serializer

---
+++ src/main/java/annotatorstub/classification/SVMLightSerializer.java
@@ -0,0 +1,11 @@
+package annotatorstub.classification;
+
+import java.io.*;
+
+public class SVMLightSerializer implements Serializable {
+
+
+    void serialize(String path) {
+        return;
+    }
+}
